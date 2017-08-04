b3b38fc59dc28f4ab2bbaeccb618c9c07abbf098
halseywu <wsh940808@163.com>
2017-08-04 09:56:46 +0800

    增加了library

---
+++ androidutilsforcxria/src/main/java/com/cxria/android/develop/utils/NetworkUtils.java
@@ -0,0 +1,41 @@
+package com.cxria.android.develop.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+/**
+ * @author 作者: Wushhhhhh
+ * @date 创建时间: 2017/8/2
+ * @description 描述: 网络相关工具类
+ * 注意权限 <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>; <uses-permission android:name="android.permission.INTERNET"/>
+ */
+public class NetworkUtils {
+    private static NetworkInfo getNetworkInfo(Context context) {
+        return ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+    }
+
+    /**
+     * 检测网络是否连接
+     *
+     * @param context 上下文
+     * @return 网络是否连接
+     */
+    public static boolean isNetworkConnected(Context context) {
+        NetworkInfo networkInfo = getNetworkInfo(context);
+        return networkInfo.isConnected();
+    }
+
+    /**
+     * 判断是不是 4G
+     * 注意添加权限 <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+     *
+     * @param context 上下文
+     * @return 当前网络是否为 4G
+     */
+    public static boolean is4G(Context context) {
+        NetworkInfo networkInfo = getNetworkInfo(context);
+        return networkInfo != null && networkInfo.isAvailable() && networkInfo.getSubtype() == TelephonyManager.NETWORK_TYPE_LTE;
+    }
+}
---
+++ androidutilsforcxria/src/main/java/com/cxria/android/develop/utils/ScreenUtils.java
@@ -0,0 +1,52 @@
+package com.cxria.android.develop.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+/**
+ * @author 作者: Wushhhhhh
+ * @date 创建时间: 2017/7/31
+ * @description 描述: 屏幕相关工具类
+ */
+public class ScreenUtils {
+    /**
+     * 获取屏幕绝对宽度「单位：px」
+     *
+     * @param context 上下文
+     * @return 屏幕宽度
+     */
+    public static int getScreenWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 获取屏幕绝对高度「单位：px」
+     *
+     * @param context 上下文
+     * @return 屏幕高度
+     */
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 获取屏幕截图
+     *
+     * @param activity 当前 Activity
+     * @return 屏幕截图
+     */
+    public static Bitmap getScreenShot(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Bitmap screenShotBitmap = Bitmap.createBitmap(bitmap, 0, 0, dm.widthPixels, dm.heightPixels);
+        view.destroyDrawingCache();
+        return screenShotBitmap;
+    }
+}
